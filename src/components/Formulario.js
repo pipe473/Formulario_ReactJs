06b5496c92232f6505b7bcfd5c644a0f2f799c17
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-04-13 18:25:31 +0200

    Creando segundo input del formulario corrigiendo la clase en skeleton

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,9 +10,18 @@ const Formulario = () => {
                 <input 
                     type="text"
                     name="mascota"
-                    className="u-full-with"
+                    className="u-full-width"
                     placeholder="Nombre Mascota"
                 />
+                <label>Nombre Dueño</label>
+                <input 
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre Dueño de la mascota"
+                />
+
+
             </form>
         </Fragment>
      );
